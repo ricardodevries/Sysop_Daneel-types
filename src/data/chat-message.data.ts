36bf3071072d860eea986f2e1331cecdfbd41378
Ricardo de Vries <78354174+ricardodevries@users.noreply.github.com>
2025-11-13 12:00:00 +0100

    Initial commit

---
+++ src/data/chat-message.data.ts
@@ -0,0 +1,20 @@
+export interface ChatMessageData {
+  userId: string;
+  username: string;
+  displayName: string;
+  messageId: string;
+  message: string;
+  logoUrl: string;
+  isMod: boolean;
+  isVip: boolean;
+  isPartner: boolean;
+  isSubscriber: boolean;
+  isBroadcaster: boolean;
+  emotes?: {
+    [emoteid: string]: string[];
+  };
+  type: string | undefined;
+  id: string | undefined;
+  teamMemberIconUrl: string | undefined;
+  isHighlighted: boolean;
+}
